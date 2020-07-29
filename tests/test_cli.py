a5eb090d17d4b52211c8a2217b9b2fa65e80cc69
Stefan Hagen <stefan@hagen.link>
2020-07-29 14:50:05 +0200

    Update test_cli.py

--- tests/test_cli.py
+++ tests/test_cli.py
@@ -5,6 +5,13 @@ import pytest  # type: ignore
 import mc_flow_sim.cli as cli
 
 
+def test_main_ok_empty_list(capsys):
+    seq = []
+    assert cli.main(seq) is None
+    out, err = capsys.readouterr()
+    assert out.strip() = str(None)
+
+
 def test_main_ok_string(capsys):
     string = "abc"
     assert cli.main(string) is None
