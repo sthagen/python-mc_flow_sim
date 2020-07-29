cc17d140271c4afee8c42d598f44570447ad2af6
Stefan Hagen <stefan@hagen.link>
2020-07-29 14:45:33 +0200

    Update test_cli.py

--- tests/test_cli.py
+++ tests/test_cli.py
@@ -7,3 +7,10 @@ import mc_flow_sim.cli as cli
 
 def test_main_ok_string():
     cli.main("abc")
+
+def test_main_ok_string_list(capsys):
+    the_same = "a"
+    seq = [the_same, the_same, the_same]
+    cli.main(seq)
+    out, err = capsys.readouterr()
+    assert out == the_same
